88efd1a95e00d6f430c4cd39702508caa903081a
SmilingWayne <593302270@qq.com>
2022-05-16 15:21:16 +0800

    20220516mod

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@
 </div>
 </template>
 
+
 <script setup>
+// TODO: 这里push 的时候要选择 git push -u secondorigin "master" 才可以！
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from "./components/HelloWorld.vue";
